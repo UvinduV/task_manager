f0e6a52c8e7558bfa1dcb59872d88e728fc66df0
UvinduV <uvindusanketh470@gmail.com>
2025-04-11 23:03:54 +0530

    create get all tasks

--- src/main/java/com/example/task_manager_backend/service/Impl/TaskServiceImpl.java
+++ src/main/java/com/example/task_manager_backend/service/Impl/TaskServiceImpl.java
@@ -10,6 +10,8 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @Transactional
 public class TaskServiceImpl implements TaskService {
@@ -25,5 +27,10 @@ public class TaskServiceImpl implements TaskService {
         if(savedTask == null) {
             throw new DataPersistExeption("Task not saved");
         }
+    }
+
+    @Override
+    public List<TaskDTO> getAllTasks() {
+        return taskMapping.asTaskDTOList(taskDao.findAll());
     }
 }
--- src/main/java/com/example/task_manager_backend/service/TaskService.java
+++ src/main/java/com/example/task_manager_backend/service/TaskService.java
@@ -2,6 +2,10 @@ package com.example.task_manager_backend.service;
 
 import com.example.task_manager_backend.dto.Impl.TaskDTO;
 
+import java.util.List;
+
 public interface TaskService {
     void saveTask(TaskDTO taskDTO);
+
+    List<TaskDTO> getAllTasks();
 }
